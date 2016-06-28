d52761c6b906c108358d1b9357abea624227d409
corecritter <bobbysavel@gmail.com>
2016-06-28 19:22:05 -0400

    Initial

---
+++ TestPlotMVVM/TestPlotMVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPlotMVVM.ViewModel
+{
+    public class MainWindowViewModel
+    {
+    }
+}
