d78db9e7a60549e91ec658253267e4959143934b
corecritter <bobbysavel@gmail.com>
2016-08-01 20:23:18 -0400

    The beginning of the plot, messy

--- MvvmLight1/CoreLibrary/Model/PointSetShape.cs
+++ MvvmLight1/CoreLibrary/Model/PointSetShape.cs
@@ -18,10 +18,22 @@ namespace CoreLibrary.Model
 
         public GeometryModel3D GetShape()
         {
-            GeometryModel3D model = new GeometryModel3D();
-            model.Material = new DiffuseMaterial(Brushes.Black);
-            model.Geometry = StandardSquare.CreateSquare();
+
+            //double worldAspect = (rangeX) / rangeY;
+            double midX = (x1 + x2) / 2.0;                      //Find midpoint of two points
+            double midY = (y1 + y2) / 2.0;
+            double length = Math.Sqrt(Math.Pow((x1-x2), 2) + Math.Pow((y1-y2), 2));
             ModelTransformations transformations = new ModelTransformations();
+            transformations.scale((length / 2), 10);                                                          //Scale x and y appropriately
+            transformations.rotate((180 / Math.PI) * Math.Atan(((y1-y2)) / (midX)));                      //Rotation angle (arcTan of height/width converted to degrees)
+            transformations.translate(midX, midY);
+
+
+
+            GeometryModel3D model = new GeometryModel3D();
+            model.Material = new DiffuseMaterial(Brushes.Green);
+            model.Geometry = StandardSquare.CreateSquare();
+            model.Transform = transformations.getTransformations();
             return model;
         }
     }
--- MvvmLight1/MvvmLight1/ViewModel/PlotViewModel.cs
+++ MvvmLight1/MvvmLight1/ViewModel/PlotViewModel.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Media.Media3D;
 
 namespace MvvmLight1.ViewModel
@@ -15,19 +17,40 @@ namespace MvvmLight1.ViewModel
         private Point3D position;
         private Vector3D at;
         private Vector3D up;
-
+        private Viewport3D viewport;
         private DataRepository _dataRepository;
+
         public PlotViewModel(DataRepository dataRepository)
         {
-            position = new Point3D(0, 0, 1);
-            at = new Vector3D(0, 0, -1);
+            position = new Point3D(-40, 40, 40);
+            at = new Vector3D(40, -40, -40);
+
+
+
+            //position = new Point3D(0, 0, 1);
+            //at = new Vector3D(0, 0, -1);
             up = new Vector3D(0, 1, 0);
-            camera = new OrthographicCamera(position, at, up, 0);
+            camera = new OrthographicCamera(position, at, up, 10);
             camera.NearPlaneDistance = 1;
             this._dataRepository = dataRepository;
         }
 
-        public ModelVisual3D Objects
+        public Viewport3D ViewPort
+        {
+            get
+            {
+                if (viewport == null)
+                {
+                    viewport = new Viewport3D();
+                }
+                viewport.Children.Clear();
+                viewport.Camera = camera;
+                //viewport.Children.Add(Objects);
+                return viewport;
+            }
+        }
+
+        public Model3DGroup Objects
         {
             get
             {
@@ -36,12 +59,117 @@ namespace MvvmLight1.ViewModel
                 foreach (var shape in this._dataRepository.GetShapes())
                 {
                     var o = shape.GetShape();
-                    if(o!=null)
-                        group.Children.Add(shape.GetShape());
+                    if (o != null)
+                       group.Children.Add(shape.GetShape());
                 }
-                objects.Content = group;
-                return objects;
+                //objects.Content = group;
+                DirectionalLight light = new DirectionalLight(Colors.White, new Vector3D(-1, -1, -3));
+                group.Children.Add(light);
+
+
+
+                
+                //group.Children.Add(m);
+                //objects.Content = group;
+                //return objects;
+                return group;
             }
         }
+        public OrthographicCamera Camera
+        {
+            get
+            {
+                return this.camera;
+            }
+        }
+        private GeometryModel3D TestObject()
+        {
+            GeometryModel3D m = new GeometryModel3D();
+            //m.Geometry = new MeshGeometry3D()
+
+            MeshGeometry3D mesh = new MeshGeometry3D();
+            //mesh.Positions = new 
+
+            Point3DCollection points = new Point3DCollection();
+            points.Add(new Point3D(0, 0, 0));
+            points.Add(new Point3D(10, 0, 0));
+            points.Add(new Point3D(10, 10, 0));
+            points.Add(new Point3D(0, 10, 0));
+            points.Add(new Point3D(0, 0, 10));
+            points.Add(new Point3D(10, 0, 10));
+            points.Add(new Point3D(10, 10, 10));
+            points.Add(new Point3D(0, 10, 10));
+
+            Int32Collection indices = new Int32Collection();
+            indices.Add(0);
+            indices.Add(1);
+            indices.Add(3);
+            indices.Add(1);
+            indices.Add(2);
+            indices.Add(3);
+            indices.Add(0);
+            indices.Add(4);
+            indices.Add(3);
+            indices.Add(4);
+            indices.Add(7);
+            indices.Add(3);
+            indices.Add(4);
+            indices.Add(6);
+            indices.Add(7);
+            indices.Add(4);
+            indices.Add(5);
+            indices.Add(6);
+            indices.Add(0);
+            indices.Add(4);
+            indices.Add(1);
+            indices.Add(1);
+            indices.Add(4);
+            indices.Add(5);
+            indices.Add(1);
+            indices.Add(2);
+            indices.Add(6);
+            indices.Add(6);
+            indices.Add(5);
+            indices.Add(1);
+            indices.Add(2);
+            indices.Add(3);
+            indices.Add(7);
+            indices.Add(7);
+            indices.Add(6);
+            indices.Add(2);
+
+            mesh.Positions = points;
+            mesh.TriangleIndices = indices;
+            m.Geometry = mesh;
+            DiffuseMaterial material = new DiffuseMaterial();
+            SolidColorBrush brush = new SolidColorBrush(Colors.Black);
+            material.Brush = brush;
+            m.Material = material;
+            return m;
+        }
+    //        <!--<Viewport3D>
+    //    <Viewport3D.Camera>
+    //        <PerspectiveCamera Position = "-40,40,40" LookDirection="40,-40,-40 " 
+    //                     UpDirection="0,0,1" />
+    //    </Viewport3D.Camera>
+    //    <ModelVisual3D>
+    //        <ModelVisual3D.Content>
+    //            <Model3DGroup>
+    //                <DirectionalLight Color = "White" Direction="-1,-1,-3" />
+    //                <GeometryModel3D>
+    //                    <GeometryModel3D.Geometry>
+    //                        <MeshGeometry3D Positions = "0,0,0 10,0,0 10,10,0 0,10,0 0,0,10 
+    //                    10,0,10 10,10,10 0,10,10"
+    //                    TriangleIndices="0 1 3 1 2 3  0 4 3 4 7 3  4 6 7 4 5 6 
+    //                                     0 4 1 1 4 5  1 2 6 6 5 1  2 3 7 7 6 2"/>
+    //                    </GeometryModel3D.Geometry>
+    //                    <GeometryModel3D.Material>
+    //                        <DiffuseMaterial Brush = "Red" />
+    //                    </ GeometryModel3D.Material >
+    //                </ GeometryModel3D >
+    //            </ Model3DGroup >
+    //        </ ModelVisual3D.Content >
+    //    </ ModelVisual3D >
+    //</ Viewport3D > -->
     }
 }
